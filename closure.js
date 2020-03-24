c9da2af836c58fed818cf5ff9929583ee8e0e28a
alexonaci <alexandru.onaci@outlook.com>
2020-03-24 10:48:39 +0200

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,8 +1,10 @@
 // Higher order functions - A function that takes another function as an argument or returns a function
 
-// Clojures - functions that return functions BUT also keep a memory of their parent function
+// Closures - functions that return functions AND also keep a memory of their parent function
 
-//Execution context has 2 parts (Execution Thread and Local Memory)
+// https://www.youtube.com/watch?v=71AtaJpJHw0
+
+// Execution context has 2 parts (Execution Thread and Local Memory)
 
 const createFunction = function(x) {
     
